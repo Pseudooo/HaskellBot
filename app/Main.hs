2c86279786549e46189552e20f95d34903de2bdb
mitchell.wright877@gmail.com <mitchell.wright877@gmail.com>
2020-08-21 17:50:22 +0100

    Added more comprehensive demo

--- app/Main.hs
+++ app/Main.hs
@@ -1,34 +1,63 @@
-{-# LANGUAGE OverloadedStrings #-}  -- allows "string literals" to be Text
+{-# LANGUAGE OverloadedStrings #-}  -- allows "strings" to be Data.Text
 
 module Main where
 
-import Control.Monad (when)
-import Data.Text (isPrefixOf, toLower, Text)
+import Control.Monad (when, forM_)
+import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
-import UnliftIO
+import UnliftIO (liftIO)
+import UnliftIO.Concurrent
 
 import Discord
 import Discord.Types
 import qualified Discord.Requests as R
 
+-- Allows this code to be an executable. See discord-haskell.cabal
 main :: IO ()
 main = do
-    userFacingError <- runDiscord $ def
-        { discordToken = "Bot NzQ2Mzk1ODQ1Mjg0MzMxNTIw"
-        , discordOnEvent = eventHandler}
-    TIO.putStrLn userFacingError
+    token <- TIO.readFile "secret.txt"
+    TIO.putStrLn $ T.append "Loaded Token: " token
 
+    t <- runDiscord $ def
+        { discordToken = token
+        , discordOnStart = startHandler
+        , discordOnEnd = liftIO $ putStrLn "Ended"
+        , discordOnEvent = eventHandler
+        , discordOnLog = \s -> TIO.putStrLn s >> putStrLn ""
+        }
+    TIO.putStrLn t
+
+-- If the start handler throws an exception, discord-haskell will gracefully shutdown
+--     Use place to execute commands you know you want to complete
+startHandler :: DiscordHandler ()
+startHandler = do
+  Right partialGuilds <- restCall R.GetCurrentUserGuilds
+
+  forM_ partialGuilds $ \pg -> do
+    Right guild <- restCall $ R.GetGuild (partialGuildId pg)
+    Right chans <- restCall $ R.GetGuildChannels (guildId guild)
+    case filter isTextChannel chans of
+      (c:_) -> do _ <- restCall $ R.CreateMessage (channelId c) "Hello! I will reply to pings with pongs"
+                  pure ()
+      _ -> pure ()
+
+-- If an event handler throws an exception, discord-haskell will continue to run
 eventHandler :: Event -> DiscordHandler ()
 eventHandler event = case event of
-       MessageCreate m -> when (not (fromBot m) && isPing (messageText m)) $ do
-               _ <- restCall (R.CreateReaction (messageChannel m, messageId m) "eyes")
-               _ <- restCall (R.CreateMessage (messageChannel m) "Pong!")
-               pure ()
-       _ -> pure ()
+      MessageCreate m -> when (not (fromBot m) && isPing m) $ do
+        _ <- restCall (R.CreateReaction (messageChannel m, messageId m) "eyes")
+        threadDelay (4 * 10^(6 :: Int))
+        _ <- restCall (R.CreateMessage (messageChannel m) "Pong!")
+        pure ()
+      _ -> pure ()
+
+isTextChannel :: Channel -> Bool
+isTextChannel (ChannelText {}) = True
+isTextChannel _ = False
 
 fromBot :: Message -> Bool
 fromBot m = userIsBot (messageAuthor m)
 
-isPing :: Text -> Bool
-isPing = ("ping" `isPrefixOf`) . toLower
+isPing :: Message -> Bool
+isPing = ("ping" `T.isPrefixOf`) . T.toLower . messageText
